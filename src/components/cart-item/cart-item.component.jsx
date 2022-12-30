1d51b175365a630047cc876fedb7d31db9ac48a6
abash26 <57720544+abash26@users.noreply.github.com>
2022-12-30 16:23:45 +0100

    Update cart-item.component.jsx

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,18 +1,29 @@
-import { CartItemContainer, ItemDetails } from './cart-item.styles';
+import { useDispatch, useSelector } from 'react-redux';
 
-const CartItem = ({ cartItem }) => {
-  const { name, imageUrl, price, quantity } = cartItem;
+import {
+  selectCartCount,
+  selectIsCartOpen,
+} from '../../store/cart/cart.selector';
+import { setIsCartOpen } from '../../store/cart/cart.action';
+
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+import { CartIconContainer, ItemCount } from './cart-icon.styles';
+
+const CartIcon = () => {
+  const dispatch = useDispatch();
+
+  const cartCount = useSelector(selectCartCount);
+  const isCartOpen = useSelector(selectIsCartOpen);
+
+  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
+
   return (
-    <CartItemContainer>
-      <img src={imageUrl} alt={`${name}`} />
-      <ItemDetails>
-        <span>{name}</span>
-        <span>
-          {quantity} x ${price}
-        </span>
-      </ItemDetails>
-    </CartItemContainer>
+    <CartIconContainer data-cy="cart" onClick={toggleIsCartOpen}>
+      <ShoppingIcon className='shopping-icon' />
+      <ItemCount data-cy="counter">{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
-export default CartItem;
+export default CartIcon;
